e39079e123d72997d3b71f3bc117c6c72e9f7f25
Red Thor <51344077+thoroftroy@users.noreply.github.com>
2022-09-28 11:57:09 -0400

    Update fatDamage.py
    Now you can easily input Enemy AC and player's speed/weight for actual useage of the calculator

--- fatDamage.py
+++ fatDamage.py
@@ -1,21 +1,19 @@
 import random
 #roll to hit
-ac = 15 #just an average ac
-attackRoll = (random.randint(1,20)+2)
-if(ac > attackRoll):
+ac = input('Armor Class of Enemy:') 
+attackRoll = (random.randint(1,20))
+if(int(ac) > attackRoll):
     print('Miss')
 else:
     print('Hit')
 
 #speed calculation (Dnd has speed in sp/6sec and I need it in mps)
 #1 ft/s = 0.681818 mph
-sp = 25 #change this to players speed
+sp = input('Players Speed:') #change this to players speed
 baseSec = 6 #this is based on the turn speed in dnd
-fts = round(sp/baseSec)
-spCalc = sp*0.681818
-
-#Damage Calculator For Borbus Roll
-
+fts = round(int(sp)/baseSec)
+spCalc = int(sp)*0.681818
+w = input('Weight of the person (lbs):')
 #Here is what his force should be
 #A throwing hammer does 1d6 bludgeoning
 #thats around 700lbs of force, that being said we can assume that for
@@ -27,11 +25,10 @@ print('Here is what it should actually be based on DND:')
 print('(This is based on the amount of force from bludgeoning and their rolls, etc)')
 print('|')
 sp = spCalc
-w = 400
-d = 985
-v = w/d
+d = 985 #average human density
+v = int(w)/d
 m = v*d
-f = m*sp
+f = m*int(sp)
 print('Speed:',sp," Weight:",w,'Density: ',d)
 print("Volume: ",v," Mass: ", m,'Force: ',f)
 #Now lets calculate damage
@@ -49,6 +46,3 @@ p2 = sum(pointsList)
 print('Borbus Damage: ',p2)
 
 #9-54 for real damage if you have a speed of 25 and a weight of 400
-
-
-
