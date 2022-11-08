8e4132b7f0b69ff6a6b63a13848f878d59aaa162
Rednave_b <51344077+thoroftroy@users.noreply.github.com>
2022-11-08 14:46:29 -0500

    Update lockTester.py
    Now it works even better, you can have longer locks and shorter locks. It isn't maxed or mined at 9.... it still crashes when you get it wrong though

--- lockTester.py
+++ lockTester.py
@@ -1,56 +1,59 @@
 import time
+listLen = 'null'
 #Alrighty this will allow a preprogrammed lock to be opened
 #the correct combo will be
 winComboDef = '123456789'
 #now lets let the user try to crack the code
-def cracking(winComboList,i):
+def cracking(winComboList,i,listLen):
     i = i + 1
-    if(i<9):
-        checkNum(i,winComboList)
+    if(i<listLen):
+        checkNum(i,winComboList,listLen)
     else:
         print('You unlocked it!')
         print('Now you can go again if you want!')
         changeLock()
         
-def checkNum(i,winComboList):
+def checkNum(i,winComboList,listLen):
     num = str(input('What is the next number? '))
     if(num == winComboList[i]):
-        print('Yes, onto the next switch!')
+        print('Yes!')
         print('')
         time.sleep(0.25)
-        cracking(winComboList,i)
+        cracking(winComboList,i,listLen)
     else:
         print('Click! The switches reset!')
         i = -1
         print('')
         time.sleep(0.25)
-        cracking(i,winComboList)
+        cracking(i,winComboList,listLen)
         
     
 #but this can be changed
 def changeLock(): 
     i = -1
-    change = input('Do you want to change preset lock? (0=yes 1=no 2=exit) ')
+    listlen = 9
+    change = str(input('Do you want to change preset lock? (0=yes 1=no 2=exit) '))
     if(change == '0'):
         print('Changing Lock...')
-        winCombo = input('Type numbers 1-9 in order of lock with no spaces (ex: 768594321) ')
+        winCombo = input('Type numbers in order of lock with no spaces (ex: 768594321 [it can be more than 9 characters]) ')
         print(winCombo,' Is the new lock Number')
         print(' ')
-        convStr(winCombo,i)
+        convStr(winCombo,i,listLen)
     elif(change == '1'):
         print('Keeping Current Lock')
         print(' ')
         winCombo = winComboDef
-        convStr(winCombo,i)
+        convStr(winCombo,i,listLen)
     elif(change == '2'):
         exit
     else:
         print('Try Again')
         changeLock()
 #Just some code to turn the string into a list
-def convStr(winCombo,i): 
+def convStr(winCombo,i,listLen): 
     winComboList = list(winCombo)
-    cracking(winComboList,i)
+    listLen = int(len(winComboList))
+    cracking(winComboList,i,listLen)
     
 
 changeLock()
