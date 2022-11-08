04239a535ced2fbc7bc11df228d51abebc9daebf
Rednave_b <51344077+thoroftroy@users.noreply.github.com>
2022-11-08 13:26:12 -0500

    Update lockTester.py
    Made it work a little bit more stably, now actually ends the program when you finish unlocking it.

--- lockTester.py
+++ lockTester.py
@@ -1,49 +1,55 @@
 import time
 #Alrighty this will allow a preprogrammed lock to be opened
 #the correct combo will be
-winComboDef = '983621574'
+winComboDef = '123456789'
 #now lets let the user try to crack the code
-def cracking(winComboList,numVal):
-    numVal = numVal + 1
-    checkNum(numVal,winComboList)
+def cracking(winComboList,i):
+    i = i + 1
+    if(i<9):
+        checkNum(i,winComboList)
+    else:
+        print('You unlocked it!')
+        exit
         
-def checkNum(numVal,winComboList):
-    num = input('What is the next number?')
-    if(num == winComboList[numVal]):
+def checkNum(i,winComboList):
+    num = str(input('What is the next number? '))
+    if(num == winComboList[i]):
         print('Yes, onto the next switch!')
-        time.sleep(1)
-        cracking(winComboList,numVal)
+        print('')
+        time.sleep(0.25)
+        cracking(winComboList,i)
     else:
         print('Click! The switches reset!')
-        numVal = -1
-        time.sleep(1)
-        cracking(numVal,winComboList)
+        i = -1
+        print('')
+        time.sleep(0.25)
+        cracking(i,winComboList)
         
     
 #but this can be changed
 def changeLock(): 
-    numVal = -1
+    i = -1
     change = input('Do you want to change preset lock? (0=yes 1=no 2=exit) ')
     if(change == '0'):
         print('Changing Lock...')
         winCombo = input('Type numbers 1-9 in order of lock with no spaces (ex: 768594321) ')
         print(winCombo,' Is the new lock Number')
         print(' ')
-        convStr(winCombo,numVal)
+        convStr(winCombo,i)
     elif(change == '1'):
         print('Keeping Current Lock')
         print(' ')
         winCombo = winComboDef
-        convStr(winCombo,numVal)
+        convStr(winCombo,i)
     elif(change == '2'):
         exit
     else:
         print('Try Again')
         changeLock()
 #Just some code to turn the string into a list
-def convStr(winCombo,numVal): 
+def convStr(winCombo,i): 
     winComboList = list(winCombo)
-    cracking(winComboList,numVal)
+    cracking(winComboList,i)
     
 
 changeLock()
