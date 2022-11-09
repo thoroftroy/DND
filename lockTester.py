34974ce8c165d57f55e89eb4d7aa283a73ccfdc8
Rednave_b <51344077+thoroftroy@users.noreply.github.com>
2022-11-09 12:35:23 -0500

    Update lockTester.py
    Can now randomize the code, it will not tell you what the answer is. Currently it can be any combo of numbers but it wont contain symbols or letters. It can be any number between 1 and 1000000000000

--- lockTester.py
+++ lockTester.py
@@ -1,9 +1,14 @@
 import time
+import random
 listLen = 'null'
 #Alrighty this will allow a preprogrammed lock to be opened
 #the correct combo will be
 winComboDef = '123456789'
 #now lets let the user try to crack the code
+def randomize(winCombo,i,listLen):
+    winCombo = str(random.randint(1,1000000000000))
+    convStr(winCombo,i,listLen)
+    
 def cracking(winComboList,i,listLen):
     i += 1
     if(i<listLen):
@@ -34,7 +39,7 @@ def checkNum(i,winComboList,listLen):
 def changeLock(): 
     i = -1
     listlen = 9
-    change = str(input('Do you want to change preset lock? (0=yes 1=no 2=exit) '))
+    change = str(input('Do you want to change preset lock? (0=yes 1=no 2=exit 3=randomize password) '))
     if(change == '0') | (change == 'yes') | (change == 'Yes'):
         print('Changing Lock...')
         winCombo = input('Type numbers in order of lock with no spaces (ex: 768594321 [it can be more than 9 characters]) ')
@@ -53,6 +58,9 @@ def changeLock():
         convStr(winCombo,i,listLen)
     elif(change == '2') | (change == 'exit') | (change == 'Exit'):
         exit
+    elif(change == '3') | (change == 'randomize') | (change == 'Randomize'):
+        winCombo = winComboDef
+        randomize(winCombo,i,listLen)
     else:
         print('Try Again')
         changeLock()
