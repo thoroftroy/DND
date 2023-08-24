f8ba70618294efdc2e20bb2c103d156cc6f030d2
Rednave_b <51344077+thoroftroy@users.noreply.github.com>
2023-08-23 22:06:58 -0400

    Update noxFight1.py
    I made it work a lot better and a little more user friendly. Also added the boss tile

--- noxFight1.py
+++ noxFight1.py
@@ -2,20 +2,20 @@ import random
 import time
 import os
 
-#This program is designed for a boss fight I am doing, The X's mark spots of dark energy which hurt the player, the O's are spots of healing energy wich heal the player and the U's are unstable ground in which the players could fall. Each turn the game state changes randmoly.
-
-
 # Function to generate random positions
 def generate_positions(num, width, height):
     return [{'x': random.randint(0, width - 1), 'y': random.randint(0, height - 1)} for _ in range(num)]
 
 # Function to print the grid
-def print_grid(width, height, players, enemies, uncommons):
+def print_grid(width, height, players, enemies, uncommons, special_char):
+    print("  " + " ".join([chr(65 + x) for x in range(width)]))  # Print letter labels
     for y in range(height):
+        print(str(y + 1).rjust(2) + " ", end="")
         for x in range(width):
             is_player = any(player['x'] == x and player['y'] == y for player in players)
             is_enemy = any(enemy['x'] == x and enemy['y'] == y for enemy in enemies)
             is_uncommon = any(uncommon['x'] == x and uncommon['y'] == y for uncommon in uncommons)
+            is_special = special_char['x'] == x and special_char['y'] == y
             
             if is_player:
                 print('X', end=' ')
@@ -23,9 +23,17 @@ def print_grid(width, height, players, enemies, uncommons):
                 print('O', end=' ')
             elif is_uncommon:
                 print('U', end=' ')
+            elif is_special:
+                print('B', end=' ')
             else:
                 print('.', end=' ')
         print()
+
+    print("\nCharacter Locations:")
+    print("X:", ", ".join([chr(65 + player['x']) + str(player['y'] + 1) for player in players]))
+    print("O:", ", ".join([chr(65 + enemy['x']) + str(enemy['y'] + 1) for enemy in enemies]))
+    print("U:", ", ".join([chr(65 + uncommon['x']) + str(uncommon['y'] + 1) for uncommon in uncommons]))
+    print("B:", chr(65 + special_char['x']) + str(special_char['y'] + 1))
 
 # Main loop
 while True:
@@ -38,23 +46,36 @@ while True:
     # Calculate the number of characters based on grid size
     num_players = max(1, int(total_grid_size * 0.02))  # 2% of grid size
     num_enemies = max(1, int(total_grid_size * 0.01))  # 1% of grid size
-    num_uncommons = max(0, int(total_grid_size * 0.005))  # 0.5% of grid size
+    num_uncommons = max(0, int(total_grid_size * 0.01))  # 1% of grid size
     
     players = generate_positions(num_players, grid_width, grid_height)
     enemies = generate_positions(num_enemies, grid_width, grid_height)
     uncommons = generate_positions(num_uncommons, grid_width, grid_height)
+    
+    # Initialize the special character 'B'
+    special_char = {'x': grid_width // 2, 'y': 0}
     
     # Loop for interacting with the game board
     while True:
         # Clear the screen
         os.system('cls' if os.name == 'nt' else 'clear')
         
-        print_grid(grid_width, grid_height, players, enemies, uncommons)
-        print("\nPress Enter to update the game board or press 'q' to change dimensions.")
+        print_grid(grid_width, grid_height, players, enemies, uncommons, special_char)
+        
+        print("\nPress Enter to update the game board, press 'j' to jump 'B', or press 'q' to change dimensions.")
         
         choice = input()
         if choice == '':
             # Generate random positions for X, O, and U characters
+            players = generate_positions(num_players, grid_width, grid_height)
+            enemies = generate_positions(num_enemies, grid_width, grid_height)
+            uncommons = generate_positions(num_uncommons, grid_width, grid_height)
+        elif choice.lower() == 'j':
+            # Move the special character 'B' randomly within 30ft
+            special_char['x'] = random.randint(max(0, special_char['x'] - 3), min(grid_width - 1, special_char['x'] + 3))
+            special_char['y'] = random.randint(max(0, special_char['y'] - 3), min(grid_height - 1, special_char['y'] + 3))
+            
+            # Randomly update the rest of the board
             players = generate_positions(num_players, grid_width, grid_height)
             enemies = generate_positions(num_enemies, grid_width, grid_height)
             uncommons = generate_positions(num_uncommons, grid_width, grid_height)
