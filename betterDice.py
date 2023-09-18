0abb526f62d991e69d9958016e1a8d8f33349f2f
Rednave_b <51344077+thoroftroy@users.noreply.github.com>
2023-09-18 17:50:42 -0400

    Create betterDice.py
    A considerably better dice roller, less options but more convineint and a better weighting system

---
+++ betterDice.py
@@ -0,0 +1,57 @@
+import random
+import time
+
+def roll_dice(num_dice, sides):
+    rolls = []
+    total = 0
+    for _ in range(num_dice):
+        roll = random.randint(1, sides)
+        rolls.append(roll)
+        time.sleep(0.05)  # Add a 0.1-second delay
+        print(f"Roll: {roll}")
+        total += roll
+    print(f"Total: {total}")
+
+def bad_weighted_dice(num_dice, sides):
+    rolls = []
+    total = 0
+    for _ in range(num_dice):
+        # Weighted towards lower values (e.g., more 1s)
+        roll = random.choices(range(1, sides+1), weights=[5]*(sides//2) + [1]*(sides//2))[0]
+        rolls.append(roll)
+        time.sleep(0.05)  # Add a 0.1-second delay
+        print(f"Roll: {roll}")
+        total += roll
+    print(f"Total: {total}")
+
+def good_weighted_dice(num_dice, sides):
+    rolls = []
+    total = 0
+    for _ in range(num_dice):
+        # Weighted towards higher values (e.g., more 8s)
+        roll = random.choices(range(1, sides+1), weights=[1]*(sides//2) + [5]*(sides//2))[0]
+        rolls.append(roll)
+        time.sleep(0.05)  # Add a 0.1-second delay
+        print(f"Roll: {roll}")
+        total += roll
+    print(f"Total: {total}")
+
+while True:
+    input_str = input("Enter the dice roll (e.g., '3d8' or 'q' to quit: ")
+    
+    if input_str.lower() == 'q':
+        break
+    
+    input_parts = input_str.lower().split()  # Convert to lowercase
+    
+    if len(input_parts) == 2 and input_parts[0] == 'bad' or input_parts[0] == 'good':
+        num_dice, sides = map(int, input_parts[1].split('d'))
+    else:
+        num_dice, sides = map(int, input_str.split('d'))
+        
+    if input_parts[0] == 'bad':
+        bad_weighted_dice(num_dice, sides)
+    elif input_parts[0] == 'good':
+        good_weighted_dice(num_dice, sides)
+    else:
+        roll_dice(num_dice, sides)
