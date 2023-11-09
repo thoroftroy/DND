1b04e5caf50dbee58e8089289219e10363dc43a9
Rednave_b <51344077+thoroftroy@users.noreply.github.com>
2023-11-09 16:18:46 -0500

    Update runeAffinityTester.py
    added some junk so its got more to it. This isn't really needed and most of it aint intuative but its nice for what I need it for

--- runeAffinityTester.py
+++ runeAffinityTester.py
@@ -12,12 +12,26 @@ def megFunc():
     currentAffinity = 0
     calculatedRunes = 0
     while calculatedRunes != numberOfRunes:
+        print("Input the affinities for rune",calculatedRunes+1)
         print("Input the Material Affinity")
         M = float(input())
         print("Input the Elemental Affinity")
         E = float(input())
         print("Input the Generic Affinity")
         G = float(input())
+        currentAffinity = M+E+G
+        calculatedRunes += 1
+        totalAffinity += currentAffinity
+
+def megFunc2():
+    global totalAffinity
+    global numberOfRunes
+    currentAffinity = 0
+    calculatedRunes = 0
+    while calculatedRunes != numberOfRunes:
+        M = 0
+        E = 0
+        G = 10
         currentAffinity = M+E+G
         calculatedRunes += 1
         totalAffinity += currentAffinity
@@ -56,9 +70,48 @@ def main():
     print("|")
     required_affinity = calculate_required_affinity(totalNumberOfRunes)
     checkAffinity()
+
+# Calculate some more stuff
+def main2():
+    # This is a bit of a lazy hacked together thing trying to make this work better.
+    global numberOfRunes
+    global totalAffinity
+    global required_affinity
+    global totalNumberOfRunes
     
+    totalAffinity = 0
+    print('Input the number of non carbon runes in the circle')
+    numberOfRunes = int(input())
+    totalNumberOfRunes = (numberOfRunes+1)+numberOfRunes
+    megFunc2()
+    print("Your total affinity for this circle is: ",totalAffinity)
+    required_affinity = calculate_required_affinity(totalNumberOfRunes)
+    checkAffinity()
+
+#mmmm numbers
+def calculate_sum(x, y):
+    result = x
+    for i in range(2, y + 1):
+        result += x / i
+    return result
+#This is to eleviate the pain of doing the calculations by hand
+def main3():
+    # Taking input for x and y
+    x = int(input("Enter the value for x: "))
+    y = int(input("Enter the value for y: "))
+
+    # Calculating and printing the result
+    result = calculate_sum(x, y)
+    print("The result of the sum is:", round(result, 2))
+
 #Always run the program
 while True != False:
     print("|")
-    main()
+    what = input()
+    if what == "main":
+        main()
+    elif what == "main2":
+        main2()
+    elif what == "main3":
+        main3()
     print("|")
